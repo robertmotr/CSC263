aeb5d42fb10f76b20657afcbb6efa91b76859b15
Robert Motrogeanu <robert.motrogeanu@mail.utoronto.ca>
2023-01-26 18:46:33 -0500

    Update median_tree_height.py

--- median_tree_height.py
+++ median_tree_height.py
@@ -7,6 +7,55 @@ University of Toronto Mississauga
 
 # Do NOT add any "import" statements
 
+# sets node to correct position in heap for a max heap
+def max_heapify_down(heap, node):
+	'''
+	>>> list = []
+	>>> retval = max_heapify_down(list, 25)
+	>>> retval
+	# fill out unit test here later
+	'''
+	for i in range(0, len(heap)):
+		# check if L + R child exist in heap
+		l_index = 2 * (i + 1) - 1
+		r_index = 2* (i + 2) - 1
+		l = l_index  > len(heap) - 1
+		r = r_index > len(heap) - 1
+
+		if l and r:
+			maxval = max(heap[i], heap[l_index], heap[r_index])
+			if heap[i] == maxval:
+				# root is largest so its already a max heap
+				return heap
+
+			elif heap[l_index] == maxval:
+				# swap with left
+				heap[i], heap[l_index] = heap[l_index], heap[i]
+				return max_heapify_down(heap, node)
+				
+
+			elif heap[r_index] == maxval:
+				# swap with right
+			
+			
+
+		elif not (l and r):
+			# already a max heap do nothing
+			pass
+
+		else:
+			if l:
+				if
+			if r:
+
+
+
+
+# sets node to correct position in heap for a min heap
+def min_heapify_down(heap, node):
+
+
+
 def median_tree_height(commands, middle):
   '''
   Pre: commands is a list of commands, middle is the middle or lower middle value of the current collection.
@@ -19,14 +68,16 @@ def median_tree_height(commands, middle):
   commands = commands.split(' ')
   initialize = commands[0].split(' ')
 
-  # sort input into lists?
+  # sort input into min_heap and max_heap lists
   for i in range(1, len(initialize)):
   	curr = str(i)
-  	if curr < median:
+  	if curr < middle:
   		min_heap.append(curr)
 
-  	elif curr > median:
+  	elif curr > middle:
   		max_heap.append(curr)
+  for i in range(floor(n/2), 1):
+  	
 
 
   # initialize the heap
