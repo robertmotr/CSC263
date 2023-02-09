14275e9a0692a5e3e81d542ff44b8bdc4d5540ec
Robert Motrogeanu <robert.motrogeanu@mail.utoronto.ca>
2023-02-09 14:00:06 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -3,32 +3,34 @@ import typing
 def swapHelper(x: BSTNode, min: int, max: int) -> List[Any]:
 	# 1. In-order traversal on BST
 	# 2. For each node, update min/max and check if it is violated
-	if x.left != None and not x.left.visited:
-		if x.left.value > x.value:
+	if x.left != None:
+		if x.left.value > x.value and x.left.visited == False:
+			x.left.visited = True
 			return [x]
-		elif x.value > max:
+		elif x.value > max and x.left.visited == False:
+			x.left.visited = True
 			return [x]
 		else:
 			max = x.value
 			return swapHelper(x.left, min, max)
-	if x.right != None and not x.right.visited:
-		if x.right.value > x.value:
+	if x.right != None:
+		if x.right.value > x.value and x.right.visited == False:
+			x.right.visited = True
 			return [x]
-		elif x.right.value < min:
+		elif x.right.value < min and x.right.visited == False:
+			x.right.visited = True
 			return [x]
 		else:
 			min = x.value
 			return swapHelper(x.right, min, max)
 
 
-def findSwapped(x: BSTNode, min: int, max: int) -> List[Any]:
+def findSwappedSubtrees(x: BSTNode, min: int, max: int) -> List[Any]:
 	# 1. Level order search
 	# 2. Check for easy BST property violations
 	# 3. For each node, update min and max and recurse
 
-	left = swapHelper(x.left, float("-inf"), float("inf"))
-	right = swapHelper(x.right, float("-inf", float("inf")))
+	first_subtree = swapHelper(x, float("-inf"), float("inf"))
+	second_subtree = swapHelper(x, float("-inf", float("inf")))
 
-	return left + right
-
-
+	return first_subtree + second_subtree
