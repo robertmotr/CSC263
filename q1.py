8cd6efcdc0ce0260cdac54c972bc0f969ba31e60
Robert Motrogeanu <robert.motrogeanu@mail.utoronto.ca>
2023-02-09 13:24:21 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,48 +1,33 @@
 import typing
 
+def swapHelper(x: BSTNode, min: int, max: int) -> List[Any]:
+	# 1. In-order traversal on BST
+	# 2. For each node, update min/max and check if it is violated
+	if x.left != None and not x.left.visited:
+		if x.left.value > x.value:
+			return [x]
+		elif x.value > max:
+			return [x]
+		else:
+			max = x.value
+			return swapHelper(x.left, min, max)
+	if x.right != None and not x.right.visited:
+		if x.right.value > x.value:
+			return [x]
+		elif x.right.value < min:
+			return [x]
+		else:
+			min = x.value
+			return swapHelper(x.right, min, max)
 
-def findSwappedHelper():
-	
 
-
-def findSwapped(x: BSTNode, min: Any, max: Any) -> List[Any]:
+def findSwapped(x: BSTNode, min: int, max: int) -> List[Any]:
 	# 1. Level order search
 	# 2. Check for easy BST property violations
 	# 3. For each node, update min and max and recurse
 
-
-	left = []
-	right = []
-	if x.left != None:
-		# If you go left, then the maximum value is the root (draw this out)
-		max = x.value
-
-		# Check for max/min violations for child
-		if x.left.value > max:
-
-	
-		# Check for easy BST violations
-		if x.left.value > x.value:
-			left = [x.left]
-		else:
-			# No violations found, keep recursing
-			left = findSwapped(x.left, min, max)
-			right = findSwapped(x.left, min, max)
-	if x.right != None:
-
-		min = x.value
-
-		if x.right.value > min:
-
-
-	
-		# Check for easy BST violations
-		if x.right.value < x.value:
-			right = [x.right]
-		else:
-			# No violations found, keep recursing
-			left = findSwapped(x.left, min, max)
-			right = findSwapped(x.left, min, max) 
+	left = swapHelper(x.left, float("-inf"), float("inf"))
+	right = swapHelper(x.right, float("-inf", float("inf")))
 
 	return left + right
 
