4adeab84a226ebb3700a68a58a7b359ef5fca787
Robert Motrogeanu <robertmotrogeanu@yahoo.ca>
2023-02-10 00:26:10 -0500

    fixed bug

--- q1_helper.py
+++ q1_helper.py
@@ -1,21 +1,21 @@
 def swapHelper(x: BSTNode, min: int, max: int) -> List[BSTNode]:
 	if x.left != None:
 		if x.left.key > x.key and x.left.visited == False:
-			x.visited = True
-			return [x]
-		elif x.key > max and x.left.visited == False:
 			x.left.visited = True
-			return [x]
+			return [x.left]
+		elif x.left.key > max and x.left.visited == False:
+			x.left.visited = True
+			return [x.left]
 		else:
 			max = x.key
 			return swapHelper(x.left, min, max)
 	if x.right != None:
 		if x.right.key > x.key and x.right.visited == False:
 			x.right.visited = True
-			return [x]
+			return [x.right]
 		elif x.right.key < min and x.right.visited == False:
 			x.right.visited = True
-			return [x]
+			return [x.right]
 		else:
 			min = x.key
 			return swapHelper(x.right, min, max)
