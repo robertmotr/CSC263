91b4606ed0f9fbcf9d6241af1b00928461e64f32
Robert Motrogeanu <robert.motrogeanu@mail.utoronto.ca>
2023-03-01 21:25:17 -0500

    some progress

--- implementations/max_heap.c
+++ implementations/max_heap.c
@@ -9,6 +9,41 @@
 // where i is one-based
 // parent: floor(i/2)
 
+/* 
+Given a pointer array to a max heap implementation, intended
+for use in bubbling down. The max heap is known to have a violation.
+Returns a pointer to the eldest child of the parent from *(array + 0). 
+Note that get_elder takes an index as a one-based number.
+Returns NULL if there is no eldest child that is bigger than the parent.
+*/
+int *get_elder_violation(MaxHeap *heap, int index) {
+    // parent has no children
+    if(2 * index > heap->size) {
+        return NULL;
+    } 
+    
+    // no right child but there is a left, so has to be left
+    if(2 * index + 1 > heap->size) {
+        if(heap->array[2 * index - 1] > heap->array[index - 1]) {
+            return (heap->array + 2 * index - 1);
+        }
+    }
+    else {
+        // parent has two children so we compare 
+        if(heap->array[2 * index - 1] > heap->array[index - 1]) {
+            return (heap->array + 2 * index - 1);
+        }
+        else if (heap->array[2 * index] > heap->array[index  - 1]) {
+            // must be right child
+            return (heap->array + 2 * index);
+        }
+    }
+    // Parent has children but has no
+    // children that are bigger than parent
+    return NULL;
+
+}
+
 // assumes array is sorted for max heap
 MaxHeap *create_heap(int *array, int size) {
     MaxHeap *heap = malloc(sizeof(MaxHeap));
@@ -17,6 +52,33 @@ MaxHeap *create_heap(int *array, int size) {
 }
 
 int extract_max(MaxHeap *heap) {
+    // Swap last element in the array with root
+    // Then bubble down the root
+    int root = heap->array[0];
+    int tmp = heap->array[heap->size - 1];
+    heap->array[heap->size - 1] = heap->array[0];
+    heap->array[0] = tmp;
+    heap->size--;
+    // Now we bubble down
+    bubble_down(heap, 1);
+    return root;
+}
+
+/*
+Bubbles down a node at i into its correct position.
+*/
+void bubble_down(MaxHeap *heap, int index) {
+    int *eldest_child = get_elder_violation(heap, index);
+    while(eldest_child != NULL) {
+        int tmp = heap->array[index - 1];
+        heap->array[index - 1] = *eldest_child;
+        *eldest_child = tmp;
+
+        eldest_child = get_elder_violation(heap, index);
+    }
+}
+
+int max(MaxHeap *heap) {
     return heap->array[0];
 }
 
--- implementations/max_heap.h
+++ implementations/max_heap.h
@@ -7,17 +7,16 @@ typedef struct {
 } MaxHeap;
 
 MaxHeap *create_heap(int *array, int size);
-
+int max(MaxHeap *heap);
 int extract_max(MaxHeap *MaxHeap);
 void increase_priority(MaxHeap *MaxHeap, int index, int value);
 void insert(MaxHeap *MaxHeap, int value);
 void max_heapify_down(MaxHeap *MaxHeap, int index);
-
-int extract_max(MaxHeap *MaxHeap);
-
 int *heap_sort(int *array, int size);
-
 MaxHeap *build_max_heap(int *array, int size);
+
+int *get_elder_violation(MaxHeap *heap, int index);
+void bubble_down(MaxHeap *heap, int index);
 
 #endif
 
